47a98e701ad4942c7d1f19c8c3c63b00dba44e03
Juily1992 <juilliet1992@gmail.com>
2025-09-22 21:03:43 +0300

    Initial commit

---
+++ src/main/java/com/skypro/flightsSelection/FlightsSelectionApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.flightsSelection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsSelectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightsSelectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/flightsSelection/FlightsSelectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.flightsSelection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsSelectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
